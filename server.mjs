999a32dfa52f464000df17167d2fcfb5683cc784
MrMuhammadHussain <mugq678@gmail.com>
2025-07-06 17:42:11 +0500

    Remove unused imports from server and client components

--- server.mjs
+++ server.mjs
@@ -1,7 +1,6 @@
 import express from 'express';
 import http from 'http';
 import { Server } from "socket.io"
-import cors from 'cors';
 import Actions from './src/Actions.js';
 
 const app = express()
