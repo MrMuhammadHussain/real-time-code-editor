35f6d37f5b27aa464c2521e7f3b6f65419b1698e
MrMuhammadHussain <mugq678@gmail.com>
2025-07-10 01:17:30 +0500

    Remove unused useState import from Editor component

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, } from 'react'
 import Codemirror from 'codemirror'
 import "codemirror/lib/codemirror.css"
 import "codemirror/theme/ayu-dark.css"
